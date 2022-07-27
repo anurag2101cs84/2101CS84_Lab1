d48fb6b43fe0e92f34b10bc548675a901786067b
Anurag <anurag_2101cs84@iitp.ac.in>
2022-07-27 23:40:11 +0530

    selection sort

--- sorting.c
+++ sorting.c
@@ -2,46 +2,46 @@
 #include <stdlib.h>
 #include <time.h>
 
-void bubble_sort(int arr[], int len);
-// void selection_sort(int arr[], int n);
+// void bubble_sort(int arr[], int len);
+void selection_sort(int arr[], int n);
 // void insertion_sort(int arr[], int n);
 
-void bubble_sort(int arr[], int n)
-{
-    int k;
-    for (int i=0; i<n; i++)
-    {
-        for (int j=i+1; j<n; j++)
-        {
-            if (arr[j]<arr[i])
-            {
-                k=arr[i];
-                arr[i]=arr[j];
-                arr[j]=k;
-            }
-        }
-    }
-}  
-
-// void selection_sort(int arr[], int n)
-// {  
-//     int k=0;
-//     while(k<n)
+// void bubble_sort(int arr[], int n)
+// {
+//     int k;
+//     for (int i=0; i<n; i++)
 //     {
-//         int small=k;
-//         for (int i=k+1; i<n; i++)
+//         for (int j=i+1; j<n; j++)
 //         {
-//             if(arr[small]>arr[i])
+//             if (arr[j]<arr[i])
 //             {
-//                 small=i;
+//                 k=arr[i];
+//                 arr[i]=arr[j];
+//                 arr[j]=k;
 //             }
 //         }
-//         int temp=arr[small];
-//         arr[small]=arr[k];
-//         arr[k]=temp;
-//         k++;
-//     }    
-// }
+//     }
+// }  
+
+void selection_sort(int arr[], int n)
+{  
+    int k=0;
+    while(k<n)
+    {
+        int small=k;
+        for (int i=k+1; i<n; i++)
+        {
+            if(arr[small]>arr[i])
+            {
+                small=i;
+            }
+        }
+        int temp=arr[small];
+        arr[small]=arr[k];
+        arr[k]=temp;
+        k++;
+    }    
+}
 
 // void insertion_sort(int arr[], int n)
 // {  
@@ -165,7 +165,7 @@ int main(){
     int a[]={5,6,7,3,1,9,2,8,4};
     int len=9;
     
-    bubble_sort(a, len);
+    selection_sort(a, len);
     
     for(int i=0;i<len-1;i++)
     printf("%d,",a[i]);
