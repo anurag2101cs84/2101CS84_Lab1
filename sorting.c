baa31ffd92348be4a2802eb67664c19972bfc05b
Anurag <anurag_2101cs84@iitp.ac.in>
2022-07-27 23:43:54 +0530

    quick sort

--- sorting.c
+++ sorting.c
@@ -4,7 +4,7 @@
 
 // void bubble_sort(int arr[], int len);
 // void selection_sort(int arr[], int n);
-void insertion_sort(int arr[], int n);
+// void insertion_sort(int arr[], int n);
 
 // void bubble_sort(int arr[], int n)
 // {
@@ -43,74 +43,76 @@ void insertion_sort(int arr[], int n);
 //     }    
 // }
 
-void insertion_sort(int arr[], int n)
-{  
-    for (int i=1; i<n; i++)
-    {
-        int current= arr[i];
-        int j=i-1;
-        while(arr[j]>current && j>=0)
-        {
-            arr[j+1]=arr[j];
-            j--;
-        }
-        arr[j+1]=current;
-    }
+// void insertion_sort(int arr[], int n)
+// {  
+//     for (int i=1; i<n; i++)
+//     {
+//         int current= arr[i];
+//         int j=i-1;
+//         while(arr[j]>current && j>=0)
+//         {
+//             arr[j+1]=arr[j];
+//             j--;
+//         }
+//         arr[j+1]=current;
+//     }
+// }
+
+void swap(int *x, int *y)
+{
+  int temp = *x;
+  *x = *y;
+  *y = temp;
 }
 
-// void swap(int *x, int *y)
-// {
-//   int temp = *x;
-//   *x = *y;
-//   *y = temp;
-// }
+void quicksort(int array[], int length)
+{
+  srand(time(NULL));
+  quicksort_recursion(array, 0, length - 1);
+}
 
-// void quicksort(int array[], int length)
-// {
-//   srand(time(NULL));
-//   quicksort_recursion(array, 0, length - 1);
-// }
+void quicksort_recursion(int array[], int low, int high)
+{
 
-// void quicksort_recursion(int array[], int low, int high)
-// {
+  if (low < high)
+  {
 
-//   if (low < high)
-//   {
+    int pivot_index = partition(array, low, high);
 
-//     int pivot_index = partition(array, low, high);
+    quicksort_recursion(array, low, pivot_index - 1);
 
-//     quicksort_recursion(array, low, pivot_index - 1);
+    quicksort_recursion(array, pivot_index + 1, high);
+  }
+}
 
-//     quicksort_recursion(array, pivot_index + 1, high);
-//   }
-// }
+int partition(int array[], int low, int high)
+{
 
-// int partition(int array[], int low, int high)
-// {
-
-//   int pivot_index = low + (rand() % (high - low));
+  int pivot_index = low + (rand() % (high - low));
   
-//   if (pivot_index != high)
-//     swap(&array[pivot_index], &array[high]);
+  if (pivot_index != high)
+    swap(&array[pivot_index], &array[high]);
   
-//   int pivot_value = array[high];
+  int pivot_value = array[high];
   
-//   int i = low; 
+  int i = low; 
 
-//   for (int j = low; j < high; j++)
-//   {
+  for (int j = low; j < high; j++)
+  {
 
-//     if (array[j] <= pivot_value)
-//     {
-//       swap(&array[i], &array[j]);
-//       i++;
-//     }
-//   }
+    if (array[j] <= pivot_value)
+    {
+      swap(&array[i], &array[j]);
+      i++;
+    }
+  }
   
-//   swap(&array[i], &array[high]);
+  swap(&array[i], &array[high]);
   
-//   return i;
-// }
+  return i;
+}
+
+
 // void merge_sort(int a[], int length)
 // {
 //   merge_sort_recursion(a, 0, length - 1);
@@ -165,7 +167,7 @@ int main(){
     int a[]={5,6,7,3,1,9,2,8,4};
     int len=9;
     
-    insertion_sort(a, len);
+    quicksort(a, len);
     
     for(int i=0;i<len-1;i++)
     printf("%d,",a[i]);
